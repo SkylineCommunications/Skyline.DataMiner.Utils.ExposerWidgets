b306f5ab483c6a5b20b4798224e295c188800ca6
Rijad Comor <136588733+RijadCo@users.noreply.github.com>
2024-06-06 15:55:36 +0200

    Added function and dom object filter section

---
+++ ExposerWidgets/Sections/FindDomObjectsWithFiltersSection.cs
@@ -0,0 +1,102 @@
+﻿namespace Skyline.DataMiner.Utils.ExposerWidgets.Sections
+{
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Core.SRM;
+    using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
+    using Skyline.DataMiner.Net.Apps.Modules;
+    using Skyline.DataMiner.Net.ManagerStore;
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
+    using Skyline.DataMiner.Net.Profiles;
+    using Skyline.DataMiner.Net.Ticketing;
+    using Skyline.DataMiner.Utils.ExposerWidgets.Filters;
+    using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+    using Skyline.DataMiner.Utils.YLE.UI.Filters;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Section for filtering dom instances.
+    /// </summary>
+    public class FindDomObjectsWithFiltersSection : FindItemsWithFiltersSection<DomInstance>
+    {
+        private readonly Label moduleId = new Label("Module ID:");
+
+        private readonly FilterSectionBase<DomInstance> idFilterSection = new GuidFilterSection<DomInstance>("Dom Instance ID", x => DomInstanceExposers.DomDefinitionId.Equal(x));
+        private readonly FilterSectionBase<DomInstance> nameFilterSection = new StringFilterSection<DomInstance>("Dom Instance Name Equals", x => DomInstanceExposers.Name.Equal(x));
+        private readonly FilterSectionBase<DomInstance> nameContainsFilterSection = new StringFilterSection<DomInstance>("Dom Instance Name Contains", x => DomInstanceExposers.Name.Contains(x));
+        private readonly FilterSectionBase<DomInstance> nameDoesntContainFilterSection = new StringFilterSection<DomInstance>("Dom Instance Name Doesn't Contain", x => DomInstanceExposers.Name.NotContains(x));
+        
+        private DomHelper domHelper;
+        private readonly TextBox moduleIdTextBox = new TextBox(string.Empty);
+
+        private static Engine engine = new Engine();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindDomObjectsWithFiltersSection"/>"/> class.
+        /// </summary>
+        public FindDomObjectsWithFiltersSection() : base()
+        {
+            moduleIdTextBox.FocusLost += ModuleIdTextBox_FocusLost;
+        }
+
+        private void ModuleIdTextBox_FocusLost(object sender, TextBox.TextBoxFocusLostEventArgs e)
+        {
+            if (e.Value != null) domHelper = new DomHelper(engine.SendSLNetMessages, e.Value);
+        }
+
+        /// <summary>
+        /// Adding filter sections on a row specified.
+        /// </summary>
+        /// <param name="row">Row position where new section should appear.</param>
+        protected override void AddFilterSections(ref int row)
+        {
+            AddWidget(moduleId, ++row, 0);
+            AddWidget(moduleIdTextBox, row, 1);
+
+            AddSection(idFilterSection, new SectionLayout(++row, 0));
+
+            AddSection(nameFilterSection, new SectionLayout(++row, 0));
+
+            AddSection(nameContainsFilterSection, new SectionLayout(++row, 0));
+
+            AddSection(nameDoesntContainFilterSection, new SectionLayout(++row, 0));
+        }
+
+
+        /// <summary>
+        /// Retrieving all items in the system based on input values.
+        /// </summary>
+        /// <returns>Collection of dom instances.</returns>
+        protected override IEnumerable<DomInstance> FindItemsWithFilters()
+        {
+            if (domHelper == null) throw new ArgumentNullException("Module is not provided, therefore Dom Helper is null.");
+
+            return domHelper.DomInstances.Read(GetCombinedFilterElement());
+        }
+
+        /// <summary>
+        /// Retrieves name of dom instance.
+        /// </summary>
+        /// <returns>Name of dom instance.</returns>
+        protected override string GetItemIdentifier(DomInstance item)
+        {
+            return item.Name;
+        }
+
+        /// <summary>
+        /// Resets filters in section to default values.
+        /// </summary>
+        protected override void ResetFilters()
+        {
+            foreach (var filterSection in GetIndividualFilters())
+            {
+                filterSection.Reset();
+            }
+
+            idFilterSection.Clear();
+            nameFilterSection.Clear();
+            nameContainsFilterSection.Clear();
+            nameDoesntContainFilterSection.Clear(); 
+        }
+    }
+}
---
+++ ExposerWidgets/Sections/FindFunctionsWithFiltersSection.cs
@@ -0,0 +1,109 @@
+﻿using Skyline.DataMiner.Core.SRM;
+using Skyline.DataMiner.Net;
+using Skyline.DataMiner.Net.Helper;
+using Skyline.DataMiner.Net.Messages;
+using Skyline.DataMiner.Net.VirtualFunctions;
+using Skyline.DataMiner.Utils.ExposerWidgets.Filters;
+using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+using Skyline.DataMiner.Utils.YLE.UI.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyline.DataMiner.Utils.ExposerWidgets.Sections
+{
+    /// <summary>
+    /// Section for filtering protocol functions.
+    /// </summary>
+    public class FindFunctionsWithFiltersSection : FindItemsWithFiltersSection<FunctionDefinition>
+    {
+        private readonly Label allActiveLable = new Label("All active functions:");
+        private readonly Label protocolNameLabel = new Label("Protocol name:");
+
+        private readonly CheckBox allActiveCheckbox = new CheckBox("Only active ones");
+        private readonly TextBox protocolNameTextBox = new TextBox(string.Empty);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindFunctionsWithFiltersSection"/>"/> class.
+        /// </summary>
+        public FindFunctionsWithFiltersSection() : base()
+        {
+        }
+
+        /// <summary>
+        /// Adding filter section in the UI.
+        /// </summary>
+        /// <param name="row">Row on which section should appear.</param>
+        protected override void AddFilterSections(ref int row)
+        {
+            AddWidget(allActiveLable, ++row, 0);
+            AddWidget(allActiveCheckbox, row, 1);
+
+            AddWidget(protocolNameLabel, ++row, 0);
+            AddWidget(protocolNameTextBox, row, 1);
+        }
+
+        /// <summary>
+        /// Filtering all protocol functions in system based on provided input.
+        /// </summary>
+        /// <returns>Collection of filtered protocol functions.</returns>
+        protected override IEnumerable<FunctionDefinition> FindItemsWithFilters()
+        {
+            List<FunctionDefinition> functionDefinitions = new List<FunctionDefinition>();
+
+            if (allActiveCheckbox.IsChecked)
+            {
+                var activeProtocolFunctionVersions = SrmManagers.ProtocolFunctionManager.GetAllProtocolFunctions(true).Select(p => p.ProtocolFunctionVersions.FirstOrDefault());
+
+                foreach (var version in activeProtocolFunctionVersions)
+                {
+                    foreach (var functionDefinition in version.FunctionDefinitions)
+                    {
+                        functionDefinitions.Add(functionDefinition);
+                    }
+                }
+
+                return functionDefinitions;
+            }
+
+            if(!protocolNameLabel.Text.IsNullOrEmpty())
+            {
+                var activeProtocolFunctionVersions = SrmManagers.ProtocolFunctionManager.GetProtocolFunctions(protocolNameLabel.Text).Select(p => p.ProtocolFunctionVersions.FirstOrDefault());
+
+                foreach (var version in activeProtocolFunctionVersions)
+                {
+                    foreach (var functionDefinition in version.FunctionDefinitions)
+                    {
+                        functionDefinitions.Add(functionDefinition);
+                    }
+                }
+
+                return functionDefinitions;
+            }
+
+            return functionDefinitions;
+        }
+
+        /// <summary>
+        /// Gets name of protocol function.
+        /// </summary>
+        /// <param name="item">Protocol function for which we want to retrieve name.</param>
+        /// <returns>Name of protocol function.</returns>
+        protected override string GetItemIdentifier(FunctionDefinition item)
+        {
+            return item.Name;
+        }
+
+        /// <summary>
+        /// Resets filters in section to default values.
+        /// </summary>
+        protected override void ResetFilters()
+        {
+            allActiveCheckbox.IsChecked = false;
+            protocolNameTextBox.Text = string.Empty;
+        }
+    }
+}
